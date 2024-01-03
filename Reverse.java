b4dbdb1fac0c9d6b10a99c3ccddc05968f904d38
nouraabassi <154346670+nouraabassi@users.noreply.github.com>
2024-01-04 00:14:18 +0200

    Update Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -1,9 +1,6 @@
 public class Reverse {
     public static void main(String[] args) {
-        if (args.length == 0) {
-            System.out.println("Please provide a string as a command-line argument.");
-            return;
-        }
+     
 
         String k = args[0];
         int length = k.length();
@@ -13,11 +10,11 @@ public class Reverse {
             System.out.print(k.charAt(i));
         }
 
-        // Finding the middle character
+        // Finding the middle character(s)
+        int mid = length / 2;
         if (length % 2 != 0) {
-            System.out.println("\nThe middle character is " + k.charAt(length / 2));
+            System.out.println("\nThe middle character is " + k.charAt(mid));
         } else {
-            int mid = length / 2;
             System.out.println("\nThe middle characters are " + k.charAt(mid - 1) + " and " + k.charAt(mid));
         }
     }
