1e58756333a8f2cd5ce7f7d93147967dbc1c9bde
nouraabassi <154346670+nouraabassi@users.noreply.github.com>
2024-01-04 00:06:02 +0200

    Update Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -1,36 +1,24 @@
-/**
- * Prints a given string, backward. Then prints the middle character in the string.
- * The program expects to get one command-line argument: A string.
- */
 public class Reverse {
-	public static void main (String[] args){
-		//// Put your code here
-  //Scanner m=new Scanner(System.in);
-  //String k = m.nextLine();
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Please provide a string as a command-line argument.");
+            return;
+        }
 
-	String k;
-	k=args[0];
-	//k = m.nextLine();
- 
-	//System.out.println();
-	 
-	int length =k.length();
-	int f=length ;
-	  
-	char first=k.charAt(0);
-	char last=k.charAt(length - 1);
-	   
-	 //  System.out.println(s.charAt(s.length() - 1)); // 'a'
-while (first!=last && length>0){
-	
-	 System.out.print(k.charAt(length - 1));
-	 length --;
-}
+        String k = args[0];
+        int length = k.length();
 
+        // Reversing the string
+        for (int i = length - 1; i >= 0; i--) {
+            System.out.print(k.charAt(i));
+        }
 
- System.out.print('\n'+"The middle character is "+k.charAt(f/2));
-
- 
-	}
-
+        // Finding the middle character
+        if (length % 2 != 0) {
+            System.out.println("\nThe middle character is " + k.charAt(length / 2));
+        } else {
+            int mid = length / 2;
+            System.out.println("\nThe middle characters are " + k.charAt(mid - 1) + " and " + k.charAt(mid));
+        }
+    }
 }
