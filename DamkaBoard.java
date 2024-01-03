0c80509bbce4c7ddfcf0805871bbe18ac8cbd2dd
nouraabassi <154346670+nouraabassi@users.noreply.github.com>
2024-01-04 00:19:26 +0200

    Update DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -1,27 +1,23 @@
-/**
- *  Gets a command-line argument n (int), and prints an n-by-n damka board.
- */
-/**
- *  Gets a command-line argument n (int), and prints an n-by-n damka board.
- */
 public class DamkaBoard {
-	public static void main(String[] args) {
-		//// Put your code here
+    public static void main(String[] args) {
+     
+            int n = Integer.parseInt(args[0]);
 
-	int n = Integer.parseInt(args[0]);
-
-         for (int i = 0; i < n; i++) {
-            for (int f = 0; f < n; f++) {
-                // Check the sum of the indices to determine whether to print an asterisk or space
-                if ((i + f) % 2 == 0) {
-                    System.out.print("* ");
-                } else {
-                    System.out.print("  ");
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if ((i + j) % 2 == 0) {
+                        System.out.print("* ");
+                    } else {
+                        System.out.print("  ");
+                    }
                 }
+                System.out.println();
             }
-            System.out.println(" ");
-        }
+      
+    
+	
+    }
 
-	}
+
 }
 
