1024317ee2df47438d1fc83fd1e0ad23df51d7eb
nouraabassi <154346670+nouraabassi@users.noreply.github.com>
2024-01-04 14:07:15 +0200

    Update InOrder.java

--- InOrder.java
+++ InOrder.java
@@ -1,12 +1,11 @@
 public class InOrder {
-  public static void main (String[] args) {
-		//// Write your code here
-		int first = (int) (Math.random() * (10-1));
-    int n = 0;
-    while (first > n) {
-      System.out.print(first + " ");
-      n = first;
-      first = (int) (Math.random() * (10-1));
+    public static void main(String[] args) {
+        int first = (int) (Math.random() * 10); 
+        int n = -1; 
+        while (first >= n) {
+            System.out.print(first + " ");
+            n = first;
+            first = (int) (Math.random() * 10); 
+        }
     }
-  }
 }
